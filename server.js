bd6ffcb0cdf76665c17c32508d01f3c9564875a8
Jordan Roberts <jdrobs19@gmail.com>
2020-11-23 19:24:49 -0700

    fetch route in add pizza js

--- server.js
+++ server.js
@@ -19,4 +19,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 //Use this to log mongo queries being executed
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on https://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
